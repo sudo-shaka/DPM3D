d1348ad72d40181c15f61918d912b9d4756c7a7f
sudo-shaka <68864205+sudo-shaka@users.noreply.github.com>
2022-04-24 20:26:47 -0400

    Update testcell.py

--- testcell.py
+++ testcell.py
@@ -8,31 +8,13 @@ import os
 
 Cell = DPM3D.Cell(x1=0,y1=0.0,z1=0.0,calA0=1.05,VertexRecursion=3,Kv = 1.0, Ka = 0.05, Kb = 0.05)
 
-#Cell.VertexFIRE(1.0,1e-4,10000,0.01)
-
-positions = Cell.Positions;
-Z = [positions[i].z for i in range(len(positions))]
-
-minz = min(Z);
-minz *= 0.8;
-Cell.nSurfacePoints = 500;
-Cell.SurfaceInit(minz);
-
-nout = 180; dt = 0.01; nsteps = 1000;
+nout = 180; dt = 0.01; nsteps = 100;
 
 for i in range(nout):
     for j in range(nsteps):
         Cell.EulerUpdate(1,dt);
         Cell.UpdateShapeForces();
-        if j % 100 ==0:
-            Cell.StickToSurface(minz,0.1)
-            Cell.SurfaceInit(minz)
-        else:
-            Cell.Crawling();
-        #Cell.StickToSurface(minz,0.1)
-        #Cell.Crawling();
-    #print(Cell.GetCalA0(),Cell.GetVolume(),Cell.GetSA())
-    #Cell.SurfaceInit(minz)
+
     print(Cell.GetVolume(),Cell.GetSA())
     fig = plt.figure(figsize=(12,12))
     ax = fig.add_subplot(projection='3d')
@@ -40,9 +22,6 @@ for i in range(nout):
     ax.set_zlim(minz,minz+2.25)
     ax.set_xlim(-1.5,1.5)
     ax.view_init(10, -120+(i))
-    #Xs = [Cell.SurfacePositions[n].x for n in range(len(Cell.SurfacePositions))]
-    #Ys = [Cell.SurfacePositions[n].y for n in range(len(Cell.SurfacePositions))]
-    #ax.scatter(Xs,Ys,[minz]*len(Cell.SurfacePositions),color='grey',alpha = 0.2)
     for tri in Cell.TriangleIndex:
         x = [Cell.Positions[j].x for j in tri];
         y = [Cell.Positions[j].y for j in tri];
