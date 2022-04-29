56dcb00cc37143561053989e9d1b007dff3be932
sudo-shaka <68864205+sudo-shaka@users.noreply.github.com>
2022-04-29 09:53:59 -0400

    Update testcell.py

--- testcell.py
+++ testcell.py
@@ -6,7 +6,7 @@ import os
 
 Cell = DPM3D.Cell(x1=0,y1=0.0,z1=0.0,calA0=1.05,VertexRecursion=2,Kv = 1.0, Ka = 0.05, Kb = 0.05)
 
-nout = 180; dt = 0.001; nsteps = 10;
+nout = 180; dt = 0.01; nsteps = 10;
 
 for i in progressbar(range(nout)):
     for j in range(nsteps):
