9062e674138f08f37f9cb3ac5122a1d4ba61d66d
sudo-shaka <68864205+sudo-shaka@users.noreply.github.com>
2022-05-26 12:30:03 -0400

    Update test_tissue.py

--- test_tissue.py
+++ test_tissue.py
@@ -8,13 +8,13 @@ Cell = DPM3D.Cell(x1=0,y1=0,z1=0,calA0=1.0, VertexRecursion=2,r0=1,Kv=1.0,Ka=1.0
 
 CellList = [Cell]*5;
 
-T = DPM3D.Tissue(Cells=CellList, phi0=0.7)
+T = DPM3D.Tissue(Cells=CellList, phi0=0.75)
 T.TissueDisperse()
-T.Kc = 1.0;
+T.Kc = 20.0;
 
 with imageio.get_writer('/tmp/out.gif',mode='I') as writer:
   for i in progressbar(range(90)):
-    T.EulerUpdate(nsteps=10,dt=0.005)
+    T.EulerUpdate(nsteps=100,dt=0.005)
     plottissue(T)
     plt.savefig('/tmp/'+str(i)+'.png', bbox_inches='tight')
     plt.close()
