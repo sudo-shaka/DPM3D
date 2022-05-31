471ebb97a486baa183a29bbfc0f15ebeb070e284
sudo-shaka <68864205+sudo-shaka@users.noreply.github.com>
2022-05-31 18:16:31 +0000

    Add files via upload

--- PlotDPM3D.py
+++ PlotDPM3D.py
@@ -57,5 +57,38 @@ def plottissue(Tissue):
             ax.set_zlim(0,Tissue.L);
             if plot:
                 ax.plot(x,y,z,color=(r1[ci],r2[ci],r3[ci]))
-            ax.scatter(x,y,z,color=(r1[ci],r2[ci],r3[ci]))
-            ax.grid(False)
+def plottissue2D(Tissue):
+    fig = plt.figure(figsize=(10,10))
+    random.seed(10)
+    r1 = random.rand(Tissue.NCELLS)
+    r2 = random.rand(Tissue.NCELLS)
+    r3 = random.rand(Tissue.NCELLS)
+    for ci in range(Tissue.NCELLS):
+        Cell = Tissue.Cells[ci];
+        for tri in Cell.TriangleIndex:
+            x = [Cell.Positions[i].x for i in tri]
+            y = [Cell.Positions[i].y for i in tri]
+            plot = True
+            for i in range(3):
+                if x[i] < 0:
+                    x[i] += Tissue.L*(round(abs(x[i]/Tissue.L))+1)
+                    plot = False
+                if y[i] < 0:
+                    y[i] += Tissue.L*(round(abs(y[i]/Tissue.L))+1)
+                    plot = False
+                    plot = False
+                if x[i] > Tissue.L:
+                    x[i] -= Tissue.L * ceil((x[i]-Tissue.L)/Tissue.L)
+                    plot = False
+                if y[i] > Tissue.L:
+                    y[i] -= Tissue.L * ceil((y[i]-Tissue.L)/Tissue.L)
+                    plot = False
+
+            xt = sorted(x);yt = sorted(y)
+            if plot == False:
+                if (abs(xt[0] - xt[-1])**2 + abs(yt[0] - yt[-1])**2) < Tissue.L:
+                    plot = True
+            if plot:
+                plt.plot(x,y,color=(r1[ci],r2[ci],r3[ci]))
+            plt.scatter(x,y,color=(r1[ci],r2[ci],r3[ci]))
+            plt.axis('equal')
