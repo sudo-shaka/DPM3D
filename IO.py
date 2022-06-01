f6a129d7bfacc05af003b338771a61d443c57f20
sudo-shaka <68864205+sudo-shaka@users.noreply.github.com>
2022-06-01 15:21:58 +0000

    Add files via upload

--- IO.py
+++ IO.py
@@ -7,3 +7,19 @@ def OutCellToFile(filename,Cell):
     out.write(str(Y)[1:-1]+"\n")
     out.write(str(Z)[1:-1]+"\n")
 
+def OutTissueToFile(filename,Tissue):
+  with open(filename,'w') as out:
+    out.write('\n')
+  out.close()
+  for ci in range(Tissue.NCELLS):
+    X = [Tissue.Cells[ci].Positions[i].x for i in range(Tissue.Cells[ci].NV)]
+    Y = [Tissue.Cells[ci].Positions[i].y for i in range(Tissue.Cells[ci].NV)]
+    Z = [Tissue.Cells[ci].Positions[i].z for i in range(Tissue.Cells[ci].NV)]
+    with open(filename,'a') as out:
+      out.write(str(X)[1:-1]+"\n")
+      out.write(str(Y)[1:-1]+"\n")
+      out.write(str(Z)[1:-1]+"\n")
+    out.close()
+  with open(filename,'a') as out:
+    out.write('\n')
+  out.close()
