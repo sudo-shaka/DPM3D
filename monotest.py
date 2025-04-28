8ce6db461e782ee1d9928320cf6ab5b014e7edab
Name <you@example.com>
2025-04-28 14:25:37 -0400

    testing update

--- monotest.py
+++ monotest.py
@@ -1,19 +1,19 @@
 import DPM3D
-from PlotDPM3D import plottissue2D
+import PlotDPM3D
 import matplotlib.pyplot as plt
 from progressbar import progressbar
 
-C1 = DPM3D.Cell(x1=8,y1=8,z1=1,calA0=1.05,VertexRecursion=2,r0=1.3,Kv=0.7,Ka=2.0,Kb=0)
-C2 = DPM3D.Cell(x1=0,y1=0,z1=1,calA0=1.05,VertexRecursion=2,r0=1.5,Kv=0.7,Ka=2.0,Kb=0)
+C1 = DPM3D.Cell(x1=8,y1=8,z1=1,calA0=1.05,VertexRecursion=3,r0=1.3,Kv=0.7,Ka=1.5,Kb=0)
+C2 = DPM3D.Cell(x1=0,y1=0,z1=1,calA0=1.05,VertexRecursion=3,r0=1.5,Kv=0.7,Ka=1.5,Kb=0)
 
 T = DPM3D.Tissue([C1,C2]*12,0.35)
 T.MonolayerDisperse()
 for ci in range(T.NCELLS):
-  T.Cells[ci].Ks = 5.0
-T.Kre = 100.0
-T.Kat = 2;
+  T.Cells[ci].Ks = 7.0
+T.Kre = 50
+T.Kat = 5;
 T.AttractionMethod = "JunctionSlip"
-nsteps = 10; nout = 25; dt = 0.01
+nsteps = 50; nout = 25; dt = 0.001
 for i in progressbar(range(nout)):
   for _ in range(nsteps):
     T.UpdateShapeForces()
@@ -21,7 +21,8 @@ for i in progressbar(range(nout)):
     for ci in range(T.NCELLS):
       T.Cells[ci].StickToSurface(0.25,1.0)
     T.EulerUpdate(dt)
-  if i == nout - 1:
-    plottissue2D(T)
-    plt.savefig("test_out.png")
+    #PlotDPM3D.plotVessel3D(T)
+  if i == nout - 1 or i == 1:
+    PlotDPM3D.plottissue2D(T)
+    plt.savefig("/tmp/"+str(i)+"test_out.png")
     plt.close()
